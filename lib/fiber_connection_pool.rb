4881bbeb4582ca4d5e1b3619926564bdb51fc262
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-08-16 21:44:18 +0200

    Make execute private

--- lib/fiber_connection_pool.rb
+++ lib/fiber_connection_pool.rb
@@ -1,7 +1,7 @@
 require 'fiber'
 
 class FiberConnectionPool
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 
   attr_accessor :saved_data
 
@@ -30,25 +30,6 @@ class FiberConnectionPool
     @saved_data.delete Fiber.current.object_id
   end
 
-  # Choose first available connection and pass it to the supplied
-  # block. This will block indefinitely until there is an available
-  # connection to service the request.
-  def execute(async,method)
-    f = Fiber.current
-
-    begin
-      conn = acquire(f)
-      retval = yield conn      
-      if !@saved_data[Fiber.current.object_id].nil?
-        @saved_data[Fiber.current.object_id]['affected_rows'] = conn.affected_rows
-      end
-      release_backup(f) if !async and method == 'query'
-      retval
-    ensure
-      release(f) if not async
-    end
-  end
-
   ##
   # avoid method_missing for most common methods
   #
@@ -74,62 +55,81 @@ class FiberConnectionPool
 
   private
 
-    # Acquire a lock on a connection and assign it to executing fiber
-    # - if connection is available, pass it back to the calling block
-    # - if pool is full, yield the current fiber until connection is available
-    def acquire(fiber)
+  # Choose first available connection and pass it to the supplied
+  # block. This will block indefinitely until there is an available
+  # connection to service the request.
+  def execute(async,method)
+    f = Fiber.current
 
-      if conn = @available.pop
-        @reserved[fiber.object_id] = conn
-        @reserved_backup[fiber.object_id] = conn
-        conn
-      else
-        Fiber.yield @pending.push fiber
-        acquire(fiber)
+    begin
+      conn = acquire(f)
+      retval = yield conn
+      if !@saved_data[Fiber.current.object_id].nil?
+        @saved_data[Fiber.current.object_id]['affected_rows'] = conn.affected_rows
       end
+      release_backup(f) if !async and method == 'query'
+      retval
+    ensure
+      release(f) if not async
     end
+  end
 
-    # Release connection from the backup hash
-    def release_backup(fiber)
-      @reserved_backup.delete(fiber.object_id)
+  # Acquire a lock on a connection and assign it to executing fiber
+  # - if connection is available, pass it back to the calling block
+  # - if pool is full, yield the current fiber until connection is available
+  def acquire(fiber)
+
+    if conn = @available.pop
+      @reserved[fiber.object_id] = conn
+      @reserved_backup[fiber.object_id] = conn
+      conn
+    else
+      Fiber.yield @pending.push fiber
+      acquire(fiber)
     end
+  end
 
-    # Release connection assigned to the supplied fiber and
-    # resume any other pending connections (which will
-    # immediately try to run acquire on the pool)
-    def release(fiber)
-      @available.push(@reserved.delete(fiber.object_id)).compact!
+  # Release connection from the backup hash
+  def release_backup(fiber)
+    @reserved_backup.delete(fiber.object_id)
+  end
 
-      if pending = @pending.shift
-        pending.resume
-      end
+  # Release connection assigned to the supplied fiber and
+  # resume any other pending connections (which will
+  # immediately try to run acquire on the pool)
+  def release(fiber)
+    @available.push(@reserved.delete(fiber.object_id)).compact!
+
+    if pending = @pending.shift
+      pending.resume
     end
+  end
 
-    # Allow the pool to behave as the underlying connection
-    #
-    # If the requesting method begins with "a" prefix, then
-    # hijack the callbacks and errbacks to fire a connection
-    # pool release whenever the request is complete. Otherwise
-    # yield the connection within execute method and release
-    # once it is complete (assumption: fiber will yield until
-    # data is available, or request is complete)
-    #
-    def method_missing(method, *args, &blk)
-      async = (method[0,1] == "a")
+  # Allow the pool to behave as the underlying connection
+  #
+  # If the requesting method begins with "a" prefix, then
+  # hijack the callbacks and errbacks to fire a connection
+  # pool release whenever the request is complete. Otherwise
+  # yield the connection within execute method and release
+  # once it is complete (assumption: fiber will yield until
+  # data is available, or request is complete)
+  #
+  def method_missing(method, *args, &blk)
+    async = (method[0,1] == "a")
 
-      execute(async,method) do |conn|
-        df = conn.send(method, *args, &blk)
+    execute(async,method) do |conn|
+      df = conn.send(method, *args, &blk)
 
-        if async
-          fiber = Fiber.current
-          df.callback do
-            release(fiber)
-            release_backup(fiber)
-          end
-          df.errback { release(fiber) }
+      if async
+        fiber = Fiber.current
+        df.callback do
+          release(fiber)
+          release_backup(fiber)
         end
-
-        df
+        df.errback { release(fiber) }
       end
+
+      df
     end
+  end
 end
