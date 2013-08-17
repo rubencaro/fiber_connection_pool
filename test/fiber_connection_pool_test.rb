3ccea4509bc43e5b1962ce2b4d7edfd3e09b90e4
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-08-17 08:31:02 +0200

    Add failure reaction testing

--- test/fiber_connection_pool_test.rb
+++ test/fiber_connection_pool_test.rb
@@ -1,5 +1,4 @@
 require 'helper'
-require 'em-synchrony'
 
 class TestFiberConnectionPool < Minitest::Test
 
@@ -29,30 +28,20 @@ class TestFiberConnectionPool < Minitest::Test
   end
 
   def test_em_synchrony_behaviour
-    a = b = nil
     info = { :threads => [], :fibers => [], :instances => []}
 
-    EM.synchrony do
-      # get pool and fibers
-      pool = FiberConnectionPool.new(:size => 5) { ::EMSynchronyConnection.new(:delay => 0.05) }
+    # get pool and fibers
+    pool = FiberConnectionPool.new(:size => 5) { ::EMSynchronyConnection.new(:delay => 0.05) }
 
-      fibers = Array.new(15){ Fiber.new { pool.do_something(info) } }
+    fibers = Array.new(15){ Fiber.new { pool.do_something(info) } }
 
-      a = Time.now
-      fibers.each{ |f| f.resume }
-      # wait all fibers to end
-      while fibers.any?{ |f| f.alive? } do
-        EM::Synchrony.sleep 0.01
-      end
-      b = Time.now
-      EM.stop
-    end
+    lapse = run_em_reactor fibers
 
     # 15 fibers on a size 5 pool, and -non-blocking- connections
     # with a 0.05 delay we expect to spend at least: 0.05*15/5 = 0.15
     # plus some breeze lost on precision on the wait loop
     # then we should be under 0.20 for sure
-    assert_operator((b - a), :<, 0.20)
+    assert_operator(lapse, :<, 0.20)
 
     # we should have visited 1 thread, 15 fibers and 5 instances
     info.dup.each{ |k,v| info[k] = v.uniq }
@@ -74,36 +63,35 @@ class TestFiberConnectionPool < Minitest::Test
   end
 
   def test_failure_reaction
-    a = b = nil
     info = { :threads => [], :fibers => [], :instances => []}
 
-    EM.synchrony do
-      # get pool and fibers
-      pool = FiberConnectionPool.new(:size => 5) { ::EMSynchronyConnection.new(:delay => 0.05) }
+    # get pool and fibers
+    pool = FiberConnectionPool.new(:size => 5) { ::EMSynchronyConnection.new(:delay => 0.05) }
 
-      fibers = Array.new(15){ Fiber.new { pool.do_something(info) } }
+    fibers = Array.new(14){ Fiber.new { pool.do_something(info) } }
 
-      a = Time.now
-      fibers.each{ |f| f.resume }
-      # wait all fibers to end
-      while fibers.any?{ |f| f.alive? } do
-        EM::Synchrony.sleep 0.01
+    failing_fiber = Fiber.new do
+      begin
+        pool.fail(info)
+      rescue
+        pool.with_failed_connection do |connection|
+          info[:repaired_connection] = connection.object_id
+        end
       end
-      b = Time.now
-      EM.stop
     end
+    # put it among others, not the first or the last
+    # so we see it does not mistake the failing connection
+    fibers.insert 7,failing_fiber
 
-    # 15 fibers on a size 5 pool, and -non-blocking- connections
-    # with a 0.05 delay we expect to spend at least: 0.05*15/5 = 0.15
-    # plus some breeze lost on precision on the wait loop
-    # then we should be under 0.20 for sure
-    assert_operator((b - a), :<, 0.20)
+    run_em_reactor fibers
 
     # we should have visited 1 thread, 15 fibers and 5 instances
     info.dup.each{ |k,v| info[k] = v.uniq }
     assert_equal 1, info[:threads].count
     assert_equal 15, info[:fibers].count
     assert_equal 5, info[:instances].count
+
+    assert_equal info[:repaired_connection], info[:failing_connection]
   end
 
 
--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,6 @@
 require 'minitest/pride'
 require 'minitest/autorun'
+require 'em-synchrony'
 
 require_relative '../lib/fiber_connection_pool'
 
@@ -11,7 +12,6 @@ class BlockingConnection
   def do_something(info)
     fill_info info
     sleep @delay
-    self.object_id
   end
 
   def fill_info(info)
@@ -29,6 +29,24 @@ class EMSynchronyConnection < BlockingConnection
 
   def fail(info)
     fill_info info
+    info[:failing_connection] = self.object_id
     raise "Sadly failing here..."
   end
+end
+
+# start an EM reactor and run given fibers
+# return time spent
+def run_em_reactor(fibers)
+  a = b = nil
+  EM.synchrony do
+    a = Time.now
+    fibers.each{ |f| f.resume }
+    # wait all fibers to end
+    while fibers.any?{ |f| f.alive? } do
+      EM::Synchrony.sleep 0.01
+    end
+    b = Time.now
+    EM.stop
+  end
+  b-a
 end
