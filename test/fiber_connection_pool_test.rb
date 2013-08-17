7c1cd1f1ecfa28642f71df205ea4a96c95c54c9f
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-08-17 07:48:25 +0200

    Add fail to mock connection

--- test/fiber_connection_pool_test.rb
+++ test/fiber_connection_pool_test.rb
@@ -1,16 +1,17 @@
-Thread.abort_on_exception = true
 require 'helper'
+require 'em-synchrony'
 
 class TestFiberConnectionPool < Minitest::Test
 
   def test_blocking_behaviour
     # get pool and fibers
     pool = FiberConnectionPool.new(:size => 5) { ::BlockingConnection.new(:delay => 0.05) }
+    info = { :threads => [], :fibers => [], :instances => []}
 
-    fibers = Array.new(15){ Fiber.new { pool.do_something } }
+    fibers = Array.new(15){ Fiber.new { pool.do_something(info) } }
 
     a = Time.now
-    result = fibers.map(&:resume)
+    fibers.each{ |f| f.resume }
     b = Time.now
 
     # 15 fibers on a size 5 pool, but -blocking- connections
@@ -20,12 +21,14 @@ class TestFiberConnectionPool < Minitest::Test
     # Also we only use the first connection from the pool,
     # because as we are -blocking- it's always available
     # again for the next request
-    assert_equal 1, result.uniq.count
+    # we should have visited 1 thread, 15 fibers and 1 instances
+    info.dup.each{ |k,v| info[k] = v.uniq }
+    assert_equal 1, info[:threads].count
+    assert_equal 15, info[:fibers].count
+    assert_equal 1, info[:instances].count
   end
 
   def test_em_synchrony_behaviour
-    require 'em-synchrony'
-
     a = b = nil
     info = { :threads => [], :fibers => [], :instances => []}
 
@@ -68,6 +71,39 @@ class TestFiberConnectionPool < Minitest::Test
     assert_raises ArgumentError do
       FiberConnectionPool.new(:size => 0) { ::BlockingConnection.new }
     end
+  end
+
+  def test_failure_reaction
+    a = b = nil
+    info = { :threads => [], :fibers => [], :instances => []}
+
+    EM.synchrony do
+      # get pool and fibers
+      pool = FiberConnectionPool.new(:size => 5) { ::EMSynchronyConnection.new(:delay => 0.05) }
+
+      fibers = Array.new(15){ Fiber.new { pool.do_something(info) } }
+
+      a = Time.now
+      fibers.each{ |f| f.resume }
+      # wait all fibers to end
+      while fibers.any?{ |f| f.alive? } do
+        EM::Synchrony.sleep 0.01
+      end
+      b = Time.now
+      EM.stop
+    end
+
+    # 15 fibers on a size 5 pool, and -non-blocking- connections
+    # with a 0.05 delay we expect to spend at least: 0.05*15/5 = 0.15
+    # plus some breeze lost on precision on the wait loop
+    # then we should be under 0.20 for sure
+    assert_operator((b - a), :<, 0.20)
+
+    # we should have visited 1 thread, 15 fibers and 5 instances
+    info.dup.each{ |k,v| info[k] = v.uniq }
+    assert_equal 1, info[:threads].count
+    assert_equal 15, info[:fibers].count
+    assert_equal 5, info[:instances].count
   end
 
 
--- test/helper.rb
+++ test/helper.rb
@@ -8,17 +8,27 @@ class BlockingConnection
     @delay = opts[:delay] || 0.05
   end
 
-  def do_something
+  def do_something(info)
+    fill_info info
     sleep @delay
     self.object_id
+  end
+
+  def fill_info(info)
+    info[:threads] << Thread.current.object_id
+    info[:fibers] << Fiber.current.object_id
+    info[:instances] << self.object_id
   end
 end
 
 class EMSynchronyConnection < BlockingConnection
   def do_something(info)
-    info[:threads] << Thread.current.object_id
-    info[:fibers] << Fiber.current.object_id
-    info[:instances] << self.object_id
+    fill_info info
     EM::Synchrony.sleep @delay
+  end
+
+  def fail(info)
+    fill_info info
+    raise "Sadly failing here..."
   end
 end
