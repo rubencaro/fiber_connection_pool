9f83eb20e86c3159ce608402ca8e80bfc299b56a
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-09-14 16:48:25 +0200

    WIP reel_mongo example

--- examples/reel_server_mongo/main.rb
+++ examples/reel_server_mongo/main.rb
@@ -4,21 +4,23 @@ require 'fiber'
 require 'fiber_connection_pool'
 require 'celluloid/autostart'
 
+require './mongo-patch.rb'
+
 class Dispatcher
   include Celluloid::IO
 
   def initialize(opts = {})
     @db_pool_size = opts[:db_pool_size] || 5
     @pool = FiberConnectionPool.new(:size => @db_pool_size) do
-      Mongo::MongoClient.new.db('bogusdb')
+      Mongo::Connection.new.db('bogusdb')
     end
     puts "DB Pool of size #{@db_pool_size} ready..."
   end
 
   def dispatch(request)
     print '.'
-    id = @pool.collection("bogus").find({ :$where => 'sleep(2000)' }).count
-    puts "Done #{Thread.current.to_s}, #{Fiber.current.to_s}"
+    res = @pool.collection('bogus').find( :$where => "sleep(2000)" ).count
+    puts "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
     request.respond :ok, "hello, world! #{Time.now.strftime('%T')}"
   end
 end
