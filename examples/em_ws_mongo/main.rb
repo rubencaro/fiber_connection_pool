2c54ac6e24f7566c88b3f2ab16eddf50faa5b166
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-09-21 18:03:08 +0200

    WIP em_ws_mongo

--- examples/em_ws_mongo/main.rb
+++ examples/em_ws_mongo/main.rb
@@ -1,13 +1,34 @@
-require 'goliath'
+require 'fiber_connection_pool'
+require 'mongo'
+require 'em-synchrony'
+require 'mongo-em-patch'
 require 'fiber'
 
-class Main < Goliath::API
+EM.run do
 
-  def response(env)
-    print '.'
-    res = db.collection('bogus').find( :$where => "sleep(2000)" ).count
-    puts "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
-    [200,{"Content-Type" => "text/html"},"hello, world! #{Time.now.strftime('%T')}"]
+  @pool = FiberConnectionPool.new(:size => 5) do
+             Mongo::Connection.new.db('bogusdb')
+           end
+
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
+    Fiber.new do
+      ws.onopen do
+        puts "Client connected"
+      end
+
+      ws.onmessage do |msg, type|
+        puts "Received message: #{msg}"
+        print '.'
+        res = db.collection('bogus').find( :$where => "sleep(2000)" ).count
+        msg = "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
+        puts msg
+        ws.send msg, :type => type
+      end
+
+      ws.onclose do
+        puts "Client disconnected"
+      end
+    end.resume
   end
 
 end
