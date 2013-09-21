595d5ccb72a5d4d74082a6e1543bbc16752b379e
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-09-21 18:48:53 +0200

    Add em_ws_mongo example

--- examples/em_ws_mongo/main.rb
+++ examples/em_ws_mongo/main.rb
@@ -1,34 +1,37 @@
+require 'websocket-eventmachine-server'
 require 'fiber_connection_pool'
 require 'mongo'
 require 'em-synchrony'
 require 'mongo-em-patch'
 require 'fiber'
 
-EM.run do
+EM.synchrony do
 
   @pool = FiberConnectionPool.new(:size => 5) do
              Mongo::Connection.new.db('bogusdb')
            end
 
-  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
-    Fiber.new do
-      ws.onopen do
-        puts "Client connected"
-      end
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 3000) do |ws|
 
-      ws.onmessage do |msg, type|
-        puts "Received message: #{msg}"
-        print '.'
-        res = db.collection('bogus').find( :$where => "sleep(2000)" ).count
+    ws.onopen do
+      puts "Client connected"
+    end
+
+    ws.onmessage do |msg, type|
+      puts "Received message: #{msg}"
+      print '.'
+      Fiber.new do
+        res = @pool.collection('bogus').find( :$where => "sleep(2000)" ).count
         msg = "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
         puts msg
         ws.send msg, :type => type
-      end
+      end.resume
+    end
 
-      ws.onclose do
-        puts "Client disconnected"
-      end
-    end.resume
+    ws.onclose do
+      puts "Client disconnected"
+    end
+
   end
 
 end
